da1afbd8a3efe4b21e8c46cd13b1d7ae4a24e1af
Finlay Lumsden <Finlay.Lumsden@softwire.com>
2025-07-08 14:34:39 +0100

    Fezz Tests Green

--- main.go
+++ main.go
@@ -10,6 +10,19 @@ func main() {
 	fmt.Println(FizzBuzz(1, 20))
 }
 
+func insertBeforeFirstB(wordsSlice []string, newWord string) (newWordsSlice []string) {
+	for wordIndex, currentWord := range wordsSlice {
+		if currentWord[0] == 'B' {
+			newWordsSlice = append(append(newWordsSlice, newWord), wordsSlice[wordIndex:]...)
+			return
+		} else {
+			newWordsSlice = append(newWordsSlice, currentWord)
+		}
+	}
+	newWordsSlice = append(newWordsSlice, newWord)
+	return
+}
+
 func FizzBuzz(minimumBound int, maximumBound int) (fbOutput string) {
 	for currentNumber := minimumBound; currentNumber <= maximumBound; currentNumber++ {
 		applicableWordsSlice := make([]string, 0)
@@ -24,6 +37,9 @@ func FizzBuzz(minimumBound int, maximumBound int) (fbOutput string) {
 		}
 		if currentNumber%11 == 0 {
 			applicableWordsSlice = []string{"Bong"}
+		}
+		if currentNumber%13 == 0 {
+			applicableWordsSlice = insertBeforeFirstB(applicableWordsSlice, "Fezz")
 		}
 		if len(applicableWordsSlice) == 0 {
 			fbOutput += strconv.Itoa(currentNumber) + "\n"
